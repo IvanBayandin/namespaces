5a4cb30290d9ee6dbc435a4d1ba143d6a77ea9d8
IvanBayandin <v.bayandin2016@outlook.com>
2024-12-11 14:52:34 +0500

    Add files via upload

---
+++ namespaces.py
@@ -0,0 +1,9 @@
+def test_function():
+    locals ()
+    def inner_function():
+        print("Я в области видимости функции test_finction")
+    inner_function()
+
+test_function()
+
+inner_fuction()
